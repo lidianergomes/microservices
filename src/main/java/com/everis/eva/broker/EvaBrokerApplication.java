013b20e23e6b770f44d4e620395cba91f9687c2c
lidianergomes <lidiane.regomes@gmail.com>
2017-09-18 18:43:31 -0300

    Primeira versao

---
+++ src/main/java/com/everis/eva/broker/EvaBrokerApplication.java
@@ -0,0 +1,12 @@
+package com.everis.eva.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaBrokerApplication.class, args);
+	}
+}
